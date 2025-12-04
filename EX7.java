29a5b3335fc612385a15373cfc36e8d06efcab10
Hkiri Arij <arijhkiri04@gmail.com>
2025-12-04 22:11:15 +0100

    Refactor EX7.java to remove redundant code

--- EX7.java
+++ EX7.java
@@ -1,19 +1,16 @@
 public class Ex7 {
     public static void main(String[] args) {
-
-        // Vérifier qu'un argument est fourni
         if (args.length == 0) {
             System.out.println("Veuillez fournir un nombre en argument.");
             return;
         }
-nvertir l'argument en entier
         int n = Integer.parseInt(args[0]);
         if (n < 2) {
             System.out.println(n + " n'est pas un nombre premier");
             return;
         }
         boolean estPremier = true;
-        for (int i = 2; i <= Math.sqrt(n); i++) { // optimisation jusqu'à sqrt(n)
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 estPremier = false;
                 break;
@@ -26,4 +23,5 @@ nvertir l'argument en entier
             System.out.println(n + " n'est pas un nombre premier");
         }
     }
+
 }
